e092bbdbf90dad2a4eeb786ed9feb85a19898bd9
janarozen <rozenbergjana@gmail.com>
2022-06-08 18:23:53 -0300

    volvimos a agregar Direccion inciial a final al tramo.

--- src/main/java/utn/ddsG8/impacto_ambiental/movilidad/Tramo.java
+++ src/main/java/utn/ddsG8/impacto_ambiental/movilidad/Tramo.java
@@ -1,5 +1,6 @@
 package utn.ddsG8.impacto_ambiental.movilidad;
 
+import utn.ddsG8.impacto_ambiental.estructura.Direccion;
 import utn.ddsG8.impacto_ambiental.movilidad.transportes.Transporte;
 import utn.ddsG8.impacto_ambiental.services.distancia.Distancia;
 
@@ -9,7 +10,25 @@ import java.time.LocalDate;
 public class Tramo {
     private LocalDate fecha;
     private Transporte transporte;
+    private Direccion DireccionFinal;
+    private Direccion DireccionInicial;
     // una solucion al tema de la orgs podria ser agregar organizacion por tramo
+
+    public Direccion getDireccionFinal() {
+        return DireccionFinal;
+    }
+
+    public void setDireccionFinal(Direccion direccionFinal) {
+        DireccionFinal = direccionFinal;
+    }
+
+    public Direccion getDireccionInicial() {
+        return DireccionInicial;
+    }
+
+    public void setDireccionInicial(Direccion direccionInicial) {
+        DireccionInicial = direccionInicial;
+    }
 
     public Tramo(Transporte transporte) {
         this.transporte = transporte;
